5fbba912286b947440292cbc758bd800ef4dc3bd
Maxwell Calkin <maxwellcalkin@gmail.com>
2023-05-03 19:17:22 -0400

    Displaying critic score

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
 
 function App() {
